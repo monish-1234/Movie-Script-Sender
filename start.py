30ce81f2c164b2a7933e403c05e4e9a7754268df
Monish <jmonishram@gmail.com>
2020-04-26 22:27:11 +0530

    added Movie Scripts

--- start.py
+++ start.py
@@ -1,9 +1,21 @@
 from pynput.keyboard import Key , Controller
 import time
 
-a=input(int("Choose any movie script :- \n 1. Test \n 2. Bee \n 3. Pirates of the Carribean "))
+a=input(int("Choose any movie script :- \n 1. Test \n 2. Bee Movie \n 3. Pirates of the Carribean \n 4. Alien Vs Predator \n 5. Wall-E \n 6. Scary Movie \n 7. Titanic \n 8. Custom File "))
 
-file = open('pirates.txt', 'r') 
+if(a==1):
+    file = open('lorem.txt', 'r') 
+else if (a==2):
+    file = open('bee.txt', 'r')
+else if (a==3):
+    file = open('pirates.txt', 'r')
+else if (a==4):
+    file = open('pirates.txt', 'r')
+else if (a==5):
+    file = open('pirates.txt', 'r')
+else if (a==6):
+    file = open('pirates.txt', 'r')
+
 time.sleep(4) 
 while 1: 
       
